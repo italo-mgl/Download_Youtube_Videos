a330e5532812052970fd82210232adfbc0bc1a02
Ítalo Magalhães <111648211+italo-mgl@users.noreply.github.com>
2024-07-22 21:54:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ while True:
     evento, valor = janela.read()
 
     if valor == sg.WIN_CLOSED:
+
+            if valor == sg.WIN_CLOSED:
+
         break
     
     elif evento == "Download MP4":
